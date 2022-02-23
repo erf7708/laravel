9add2d31d049a9073543a398f53eb2d5a4f1b2d9
OmidMehri <omid.mdkc@gmai.com>
2022-02-23 16:34:45 +0330

    change main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -41,9 +41,9 @@
    * Sidebar toggle
    */
   if (select('.toggle-sidebar-btn')) {
-    on('click', '.toggle-sidebar-btn', function(e) {
-      select('body').classList.toggle('toggle-sidebar')
-    })
+      on('click', '.toggle-sidebar-btn', function(e) {
+        select('body').classList.toggle('toggle-sidebar')
+      })
   }
 
   /**
